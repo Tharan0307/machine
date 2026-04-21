031504631ab7d3015cf43b4e58cadbbe7c3c3654
Tharan0307 <kumartharan982@gmail.com>
2026-04-21 11:51:49 +0530

    small change

--- backend/database.py
+++ backend/database.py
@@ -5,7 +5,7 @@ def get_connection():
     connection = mysql.connector.connect(
         host="localhost",        # MySQL server host
         user="root",             # MySQL username
-        password="your-password", # ← Change this to your MySQL password
+        password="Tharan@2004", # ← Change this to your MySQL password
         database="machine_db"   # Database name
     )
     return connection
